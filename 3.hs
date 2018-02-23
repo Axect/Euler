1f6c6d6c7662389f67261daa35165f055b52812f
Axect <edeftg@gmail.com>
2018-02-23 22:17:15 +0900

    Hmm..?

--- 3.hs
+++ 3.hs
@@ -15,7 +15,7 @@ isPrime' x = all (\p -> x `rem` p > 0) (factorsToTry x)
 
 primeFactors' n = filter (\p -> mod n p == 0) (takeWhile (\p -> p*p <= n) primes')
 
--- Accelerate!
+-- Accelerate! (if m become smaller than p^2 then stop iteration!)
 factors :: Integral a => a -> [a] -> [a]
 factors 1 _ = []
 factors m (p:ps) | m < p*p = [m]
