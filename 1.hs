e790f833f04724268e50ec9d46a4e9246c8f4d7e
Axect <edeftg@gmail.com>
2018-02-23 22:31:19 +0900

    Fix 1 to more simple

--- 1.hs
+++ 1.hs
@@ -1,8 +1,7 @@
 module Multiple where
 
 main :: IO()
-main = do
-  print $ sumMultiple 999
+main = print $ sumMultiple 999
 
 sumMultiple :: Int -> Int
 sumMultiple n = sum [x | x <- [1..n], x`mod`3==0 || x`mod`5==0]
