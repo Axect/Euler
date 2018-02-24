8ee3317884ad141e8005d9573c5629629159c5d7
Axect <edeftg@gmail.com>
2018-02-24 10:57:13 +0900

    Fix indent

--- 2_2.hs
+++ 2_2.hs
@@ -3,5 +3,5 @@ module EvenFibo where
 main = print $ evenFibo 4000000
 
 evenFibo n = sum [x | x <- takeWhile (<=n) fibs, even x]
-    where
-        fibs = 1:2:zipWith (+) fibs (tail fibs)
+  where
+    fibs = 1:2:zipWith (+) fibs (tail fibs)
