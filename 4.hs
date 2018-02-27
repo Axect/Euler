d7121b8a6aae65f6cf66ac0b09d7f24c4479b6b5
Axect <edeftg@gmail.com>
2018-02-27 20:55:48 +0900

    Palindrome - Prob 4

--- 4.hs
+++ 4.hs
@@ -1,10 +1,22 @@
 module Palindrome where
 
 main :: IO()
-main = do
+main = print $ maximum (findPalindrome 3)
 
-revNumList :: Int -> [Int]
-revNumList n = if r/=0 then r : revNumber q else [] where (q,r) = quotRem n 10
+revNum :: Integer -> Integer
+revNum n = if q > 0 then r * 10^(1 + floor (logBase 10 (fromInteger q))) + revNum q else r
+  where (q,r) = quotRem n 10
 
-revNum :: Int -> Int
-revNum n =
+isPalindrome :: Integer -> Bool
+isPalindrome n = n == revNum n
+
+type Digit = Integer
+
+isProduct :: Integer -> Digit -> Bool
+isProduct n d
+  | n<10^(d-1) = False
+  | length [x | x <- [10^(d-1)..(10^d - 1)], let (q,r) = quotRem n x in (q >= 10^(d-1) && q < 10^d && r == 0)]==0 = False
+  | otherwise = True
+
+findPalindrome :: Digit -> [Integer]
+findPalindrome d = [x | x <- [100^(d-1)..(10^d - 1)^2], isPalindrome x, isProduct x d]
