562da7ba799dc89c951e3fae813af16241e899f0
Axect <edeftg@gmail.com>
2018-03-13 19:47:58 +0900

    Fix evenFibo

--- 2_2.hs
+++ 2_2.hs
@@ -1,7 +1,8 @@
 module EvenFibo where
 
+import Data.List
+
 main = print $ evenFibo 4000000
 
-evenFibo n = sum [x | x <- takeWhile (<=n) fibs, even x]
-  where
-    fibs = 1:2:zipWith (+) fibs (tail fibs)
+evenFibo n = foldl' (+) 0 [x | x <- takeWhile (<=n) fibs, even x]
+     where fibs = 1:2:(zipWith (+) fibs (tail fibs))
