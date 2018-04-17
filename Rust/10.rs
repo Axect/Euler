9ba762c08e7d9f3fb207d2d1d17ac1ae3f64c4ea
Axect <edeftg@gmail.com>
2018-04-17 14:49:47 +0900

    Euler 10 - Rust
    Former-commit-id: 35c79a9a28aa6151e3472637ed700ad9463888a5

---
+++ Rust/10.rs
@@ -0,0 +1,15 @@
+mod prime;
+
+use prime::Prime;
+
+pub fn main() {
+    let result: u64 = Prime { num: 1 }
+    .take_while(|&x| x < 2000000)
+    .sum();
+
+    println!("Result: {}", result);
+}
+
+fn gen_prime() -> Prime {
+    Prime { num: 1 }
+}
