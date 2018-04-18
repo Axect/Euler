a98cce5ea5ca5e26f4a7e1cfa026f53e4b4238dd
axect <edeftg@gmail.com>
2018-04-19 03:04:42 +0900

    Euler 11 - Make Trait
    Former-commit-id: 2400bbb64202cef3d7d9e1a6f575cbb40a19927c

--- Rust/euler_11.rs
+++ Rust/euler_11.rs
@@ -11,5 +11,83 @@ pub fn main() {
         .filter(|ref x| *x != &vec![""])
         .collect::<Vec<Vec<&str>>>();
 
-    println!("{:?}", result);
+    let mat = gen_matrix(result);
+
+    let (h, v) = (mat.h_product(), mat.v_product());
+
+    println!("{:?}, {:?}", h, v);
+    println!("{:?}", diagonal(mat));
+}
+
+// Type Aliases
+type Row = Vec<u64>;
+type Matrix = Vec<Vec<u64>>;
+
+trait MatProd {
+    fn h_product(&self) -> u64;
+    fn v_product(&self) -> u64;
+}
+
+// Matrix Tool
+fn gen_matrix(str_array: Vec<Vec<&str>>) -> Matrix {
+    str_array.iter()
+        .map(|x| x.iter().map(|t| t.to_string().parse::<u64>().unwrap()).collect::<Row>())
+        .collect::<Matrix>()
+}
+
+fn transpose(mat: Matrix) -> Matrix {
+    let mut a = mat.clone();
+    for i in 0..mat.len() {
+        for j in 0..mat[i].len() {
+            a[i][j] = mat[j][i];
+        }
+    }
+    a
+} 
+
+fn diagonal(mat: Matrix) -> Row {
+    let mut row: Vec<u64> = Vec::new();
+    for i in 0..mat.len() {
+        row.push(mat[i][i]);
+    }
+    row
+}
+
+// Product Tool
+fn prod_row(row: Row) -> u64 {
+    let mut largest = 1;
+    for i in 0..row.len()-4 {
+        let mut prod = 1;
+        for j in i..i+4 {
+            prod *= row[j];
+            if prod > largest {
+                largest = prod;
+            }
+        }
+    }
+    largest
+}
+
+/*
+fn h_product(mat: Matrix) -> u64 {
+    mat.iter()
+        .map(|v| prod_row(v.to_vec()))
+        .max().unwrap()
+}
+
+fn v_product(mat: Matrix) -> u64 {
+    h_product(transpose(mat))
+}
+*/
+
+impl MatProd for Matrix {
+    fn h_product(&self) -> u64 {
+        self.iter()
+            .map(|v| prod_row(v.to_vec()))
+            .max().unwrap()
+    }
+
+    fn v_product(&self) -> u64 {
+        transpose(self.to_vec()).h_product()
+    }
 }
