53cb895c1720175ff99cb64d964068d4e49a170c
axect <edeftg@gmail.com>
2018-04-18 23:06:56 +0900

    Euler11 - Rust - incomplete
    Former-commit-id: 382f8bcd7e4d79ca84e3db9c2c0279d90716b07c

---
+++ src/euler_11.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn main() {
+    let mut f = File::open("data/11.txt").expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+    let result = contents.split(" ");
+    println!("{:?}", result)
+}
