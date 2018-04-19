5866c4df4e83ffb3e6594aa7ba1908650be6598e
axect <edeftg@gmail.com>
2018-04-19 18:43:53 +0900

    Incomplete Euler 13 - Rust
    Former-commit-id: 1af3ae5834d9ce808a6c058a98968f5188691d1c

--- Rust/euler_13.rs
+++ Rust/euler_13.rs
@@ -1,15 +1,32 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::ops::Deref;
 
 pub fn main() {
     let mut f = File::open("data/13.txt").expect("There is no file here");
     let mut texts = String::new();
     f.read_to_string(&mut texts)
         .expect("Something went wrong reading the file");
-    let mat = texts.split('\n')
+    let result = texts.split('\n')
         .filter(|&x| x != "")
-        .map(|x| x.chars().take(10).collect::<String>().parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
+        .collect::<Vec<&str>>();
+    
+    let ls = large_sum(result);
 
-    println!("{:?}", mat)
+    println!("{:?}", ls);
+}
+
+fn large_sum(str_array: Vec<&str>) -> Vec<u64> {
+    let mut mat = str_array.clone();
+    let mut times = str_array[0].len();
+    let mut sums: Vec<u64> = Vec::new();
+
+    if times > 18 {
+        let s: u64 = str_array().into_iter()
+            .map(|x| x.to_string().chars().take(18).into_iter().collect::<String>()
+                 .parse::<u64>().unwrap())
+            .sum();
+        sums.push(s);
+    }
+    sums
 }
