c86ecec5c5a81eb5f8da75a620b52a77b058b897
axect <edeftg@gmail.com>
2018-04-25 18:33:19 +0900

    Euler 11 - Haskell - diagonal
    Former-commit-id: 9c540df692336368376a07e55b8d3d30285ac1fb [formerly 9c540df692336368376a07e55b8d3d30285ac1fb [formerly 5983fd2e612b5fa54bbed0a5add7c6d7b49e280e]]
    Former-commit-id: 97eaa53840db6a123e1356b883b195c587cb48f3
    Former-commit-id: d9b0c5a70955f6404df4f4a99df7d5dd9e68c002

---
+++ Haskell/11.hs
@@ -0,0 +1,45 @@
+import Data.List.Split
+
+main :: IO ()
+main = do
+  dat <- readFile "../Data/11.txt"
+  let strArray = map (splitOn " ") $ filter (/= "") $ splitOn "\n" dat
+      matArray = map (map read) strArray :: [[Integer]]
+  print $ hProdMat matArray
+  print $ vProdMat matArray
+  print $ dProdMat matArray
+
+type Vector = [Integer]
+type Matrix = [[Integer]]
+
+-- Product Code
+hProd :: Vector -> Integer
+hProd vec = (maximum . (map product))
+  [ take 4 $ drop i vec | i <- [0 .. (l - 5)] ]
+  where l = length vec
+
+hProdMat :: Matrix -> Integer
+hProdMat = maximum . (map hProd)
+
+vProdMat :: Matrix -> Integer
+vProdMat = maximum . (map hProd) . transpose
+
+dProdMat :: Matrix -> Integer
+dProdMat = maximum . (map hProd) . (map diag) . cyclics
+
+-- Matrix Code
+transpose :: Matrix -> Matrix
+transpose m = map (\l -> map (!! l) m) [0 .. (length m - 1)]
+
+diag :: Matrix -> Vector
+diag m = zipWith (!!) m [0 .. (length m - 1)]
+
+cyclic :: Matrix -> Matrix
+cyclic m = tail m ++ [head m]
+
+cyclics :: Matrix -> [Matrix]
+cyclics m = map (cyclic' m) [0 .. length m - 1]
+ where
+  cyclic' :: Matrix -> Int -> Matrix
+  cyclic' m n | n == 0    = m
+              | otherwise = cyclic' (cyclic m) (n - 1)
