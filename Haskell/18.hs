ba0a2c4b088ab37394876db280e3eccfd87054e2
axect <edeftg@gmail.com>
2018-04-27 01:28:22 +0900

    Euler 18 - Haskell
    Former-commit-id: a0cb9bddb275dd3d197800d8fd8f2d1874c97306 [formerly a0cb9bddb275dd3d197800d8fd8f2d1874c97306 [formerly a8f1164034e93091e629259e493b1441ab71764f]]
    Former-commit-id: 907990a466e3abddb1b762d6dc18c7c23af533ba
    Former-commit-id: 927f80b5ecaa290e824cc1cf746f599916e72af3

---
+++ Haskell/18.hs
@@ -0,0 +1,17 @@
+import Data.List.Split
+
+main :: IO ()
+main = do
+  r <- readFile "../Data/18.txt"
+  let strArray = map (splitOn " ") (filter (/= "") $ splitOn "\n" r)
+      numArray = map (map read) strArray :: [[Integer]]
+      revArray = reverse numArray
+  print $ reduceTotal (revArray)
+
+reduce :: [Integer] -> [Integer]
+reduce (_:[]) = []
+reduce (x:xs) = max x (head xs) : reduce xs
+
+reduceTotal :: [[Integer]] -> Integer
+reduceTotal [[x] ] = x
+reduceTotal (x:xs) = reduceTotal (zipWith (+) (reduce x) (head xs) : tail xs)
