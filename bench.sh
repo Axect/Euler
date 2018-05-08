c88563a950396fba3cbf76e106523c79646923aa
axect <edeftg@gmail.com>
2018-05-08 16:20:20 +0900

    Hyperfine integrated
    Former-commit-id: 8f676a8af49b318edf7f17a8c20a4866f1592ca0 [formerly 8f676a8af49b318edf7f17a8c20a4866f1592ca0 [formerly 642a4a970e7d421f19c34d7e8770e7501057f929]]
    Former-commit-id: d9e0e7d1a98a0ef9b110b2093fb2a553c59c5678
    Former-commit-id: 175cade4feaf6d208c336d3355b4c2c9a116d705

---
+++ bench.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ $1 -ge 1 -a $1 -lt 10 ]; then
+  num="00$1"
+elif [ $1 -ge 10 -a $1 -lt 100 ]; then
+  num="0$1"
+else
+  num="$1"
+fi
+
+hyperfine -w 1 --export-markdown ./Bench/euler${num}.md "./Rust/bin/Euler$1" "./Nim/bin/p${num}" "./Haskell/bin/$1"
--- utils/bench.nim
+++ utils/bench.nim
@@ -1,12 +1,8 @@
 import os, times, osproc, strutils, strformat, algorithm, sequtils
 import csvtools
 
-type BenchMark = object
-  lang: string
-  time: string
-
 let num = parseInt(paramStr(1))
-let repeat = parseInt(paramStr(2))
+#let repeat = parseInt(paramStr(2))
 
 let rust = &"./Rust/bin/Euler{num}"
 let haskell = &"./Haskell/bin/{num}"
@@ -17,49 +13,13 @@ of 1..9: nim = &"./Nim/bin/p00{num}"
 of 10..100: nim = &"./Nim/bin/p0{num}"
 else: nim = &"./Nim/bin/p{num}"
 
-proc measureTime(cmd: string): float =
-  let output = splitLines(cmd)
-  let temp = output[2].rsplit("m")
-  let temp2 = temp[1].rsplit("s")
-  result = parseFloat(temp2[0])
-  return result
-
 proc main =
-  var rust_time = 0.0
-  var nim_time = 0.0
-  var haskell_time = 0.0
-  
-  for i in 1 .. repeat:
-    rust_time += measureTime(execProcess("time " & &"{rust}"))
-    nim_time += measureTime(execProcess("time " & &"{nim}"))
-    haskell_time += measureTime(execProcess("time " & &"{haskell}"))
+  let output = case num
+  of 1..9: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler00{num}.md " & rust & " " & nim & " " & haskell)
+  of 10..99: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler0{num}.md " & rust & " " & nim & " " & haskell)
+  else: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler{num}.md " & rust & " " & nim & " " & haskell)
 
-  rust_time /= repeat.float
-  nim_time /= repeat.float
-  haskell_time /= repeat.float
-
-  let bench_head = BenchMark(lang: "Language", time: "Time (sec)")
-
-  let rust_bench = BenchMark(lang: "Rust", time: $rust_time)
-  let nim_bench = BenchMark(lang: "Nim", time: $nim_time)
-  let haskell_bench = BenchMark(lang: "Haskell", time: $haskell_time)
-
-  var bench_result: seq[BenchMark] = @[rust_bench, nim_bench, haskell_bench]
-  bench_result.sort do (x, y: BenchMark) -> int:
-    result = cmp(x.time, y.time)
-    if result == 0:
-      result = cmp(x.lang, y.lang)
-
-  bench_result = concat(@[bench_head], bench_result)
-
-  echo &"Rust:    {rust_time}s"
-  echo &"Nim:     {nim_time}s"
-  echo &"Haskell: {haskell_time}s"
-
-  case num
-  of 1..9: bench_result.writeToCsv(&"Bench/euler00{num}.csv")
-  of 10..99: bench_result.writeToCsv(&"Bench/euler0{num}.csv")
-  else: bench_result.writeToCsv(&"Bench/euler{num}.csv")
+  echo output
 
 if isMainModule:
   main()
