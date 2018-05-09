368166bb076658a3a85164e526b88d7d7dfed5f9
axect <edeftg@gmail.com>
2018-05-09 19:11:35 +0900

    Haskell Plot failed
    Former-commit-id: 909322ed4bdb81821b26f8e2db9cc24f586afcaa [formerly 909322ed4bdb81821b26f8e2db9cc24f586afcaa [formerly 1b33b269c4483175f6082a768e0e191fc9095755]]
    Former-commit-id: abe746b0a0828a066bcec3ddbf88be5a0ae9a991
    Former-commit-id: 218167800b0ca22a33157b34fb2637262088ce6b

--- utils/graph.hs
+++ utils/graph.hs
@@ -1,33 +1,29 @@
-{-# LANGUAGE ConstraintKinds #-}
-
-import Graphics.Rendering.Chart.Easy
-import Graphics.Rendering.Chart.Backend.Cairo
+import Graphics.EasyPlot
 import Data.List -- sort
 import Data.List.Split -- splitOn
 import System.Directory -- listDirectory
 
---main = toFile def "Bench/euler.png" $ do
---  layout_title .= "Euler Benchmark"
---  plot (line "Haskell" haskell)
---  plot (line "Nim" nim)
---  plot (line "Rust" rust)
-
-main :: IO ()
 main = do
-  mining
+  scanDir <- listDirectory "Bench/toPlot/"
+  proto0  <- mapM readFile $ map ("Bench/toPlot/" ++) (sort scanDir)
+  let proto1  = map ((filter (/= "")) . (splitOn "\n")) proto0
+      proto2  = map (sort . (drop 1)) proto1
+      proto3  = map (map (readDouble . (!! 1) . (splitOn ","))) proto2
+      haskell = zip [1.0 ..] (map (!! 0) proto3)
+      nim     = zip [1.0 ..] (map (!! 1) proto3)
+      rust    = zip [1.0 ..] (map (!! 2) proto3)
+  plot X11
+    $ [ Data2D [Title "Haskell", Style Linespoints] [] haskell
+      , Data2D [Title "Nim", Style Linespoints]     [] nim
+      , Data2D [Title "Rust", Style Linespoints]    [] rust
+      ]
+
+
 
 type Until = Int
 type Points = [(Double, Double)]
-type Data = [Points]
-
---mining :: Until -> Points
-mining = do
-  scanDir <- listDirectory "Bench/"
-  proto0  <- mapM readFile $ map ("Bench/" ++) (sort scanDir)
-  let proto1 = map ((filter (/= "")) . (splitOn "\n")) proto0
-      proto2 = map (sort . (drop 1)) proto1
-      proto3 = map (map (readDouble . (!! 1) . (splitOn ","))) proto2
-  print $ proto3
+type Data = (Points, Points, Points)
 
 readDouble :: String -> Double
 readDouble str = read str :: Double
+
