7877bd6c39751941b5f0835a82cf7c652a3e5e49
axect <edeftg@gmail.com>
2018-05-09 19:40:09 +0900

    Generate CSV Bench data with Haskell
    Former-commit-id: 13af9bf02b7c6f09ee87aa31fc3d062a821bcdaf [formerly 13af9bf02b7c6f09ee87aa31fc3d062a821bcdaf [formerly e890e4562e101dac9811c61a0bb88e8e9b2bc83c]]
    Former-commit-id: 5d96541c0e63a825517d51ba85a42aa671e639ec
    Former-commit-id: 5d3694f04d9b5cef2663f54b150d671df456848b

--- utils/graph.hs
+++ utils/graph.hs
@@ -1,28 +1,32 @@
-import Graphics.EasyPlot
+import Data.Csv
+import qualified Data.ByteString.Lazy as BSL
+
 import Data.List -- sort
 import Data.List.Split -- splitOn
 import System.Directory -- listDirectory
 
 main = do
-  scanDir <- listDirectory "Bench/toPlot/"
-  proto0  <- mapM readFile $ map ("Bench/toPlot/" ++) (sort scanDir)
+  (haskell, nim, rust) <- mining
+  BSL.writeFile "Bench/toPlot/haskell.csv" $ encode haskell
+  BSL.writeFile "Bench/toPlot/nim.csv" $ encode nim
+  BSL.writeFile "Bench/toPlot/rust.csv" $ encode rust
+
+
+type Until = Int
+type Points = [(Double, Double)]
+type Data = (Points, Points, Points)
+
+mining :: IO Data
+mining = do
+  scanDir <- listDirectory "Bench/data/"
+  proto0  <- mapM readFile $ map ("Bench/data/" ++) (sort scanDir)
   let proto1  = map ((filter (/= "")) . (splitOn "\n")) proto0
       proto2  = map (sort . (drop 1)) proto1
       proto3  = map (map (readDouble . (!! 1) . (splitOn ","))) proto2
       haskell = zip [1.0 ..] (map (!! 0) proto3)
       nim     = zip [1.0 ..] (map (!! 1) proto3)
       rust    = zip [1.0 ..] (map (!! 2) proto3)
-  plot X11
-    $ [ Data2D [Title "Haskell", Style Linespoints] [] haskell
-      , Data2D [Title "Nim", Style Linespoints]     [] nim
-      , Data2D [Title "Rust", Style Linespoints]    [] rust
-      ]
-
-
-
-type Until = Int
-type Points = [(Double, Double)]
-type Data = (Points, Points, Points)
+  return (haskell, nim, rust)
 
 readDouble :: String -> Double
 readDouble str = read str :: Double
