974293ae504f9fded391a7271c34f1d7a48be04b
axect <edeftg@gmail.com>
2018-05-10 17:55:00 +0900

    Euler 9 - Nim & Haskell fix
    Former-commit-id: 2beeb5ee41ec61cb81034f7435046da0a4fa5349 [formerly 2beeb5ee41ec61cb81034f7435046da0a4fa5349 [formerly 79c0b8adce53ac8e9177a651f58ddbc3f2fabe58]]
    Former-commit-id: bf7c17c02a227e6d57e36041c6692865ef8ea9fd
    Former-commit-id: 1314d66545cfc0564f66c385a4c36c58383f6c5f

--- Haskell/9.hs
+++ Haskell/9.hs
@@ -3,7 +3,7 @@ main = do
   let result = head
         [ a * b * (1000 - a - b)
         | a <- [1 .. 999]
-        , b <- [1 .. 999]
+        , b <- [1 .. a]
         , a ^ 2 + b ^ 2 == (1000 - a - b) ^ 2
         ]
   print $ result
---
+++ Nim/p009.nim
@@ -0,0 +1,11 @@
+import math, sequtils, future
+
+proc main =
+  for c in 1..1000:
+    for a in 1..c:
+      if c^2 == a^2 + (1000 - a - c)^2:
+        echo a*(1000 - a - c)*c
+
+if isMainModule:
+  main()
+
