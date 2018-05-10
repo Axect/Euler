86f3e93b7040dbfdbce548dfe6589c8f0349fce6
axect <edeftg@gmail.com>
2018-05-10 17:43:59 +0900

    Rust Fix
    Former-commit-id: 424530f0b3c1e825979cbfaa30aa6a3ff379fe48 [formerly 424530f0b3c1e825979cbfaa30aa6a3ff379fe48 [formerly 1a4e4e673d0243c8fe4a9f8d4ad925b6929afe45]]
    Former-commit-id: 89c7d4bcfdbf3f08c2a65003e13a70088ff8aa93
    Former-commit-id: 6200c18e9655e67d7b39b6ea8441ec3fa41c1dea

--- Rust/euler_8.rs
+++ Rust/euler_8.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn main() {
-    let mut f = File::open("../Data/8.txt").expect("file not found");
+    let mut f = File::open("Data/8.txt").expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect(
         "Something went wrong reading the file",
