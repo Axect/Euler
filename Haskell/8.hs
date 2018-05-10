21150a6513a4301c92cb17ca69542262e1d5650d
axect <edeftg@gmail.com>
2018-05-10 17:43:20 +0900

    Haskell 8 Fix
    Former-commit-id: 882ff58cddc4ab1631176c4f8c8efc4303bcaf3b [formerly 882ff58cddc4ab1631176c4f8c8efc4303bcaf3b [formerly 0d265341fe4dbf75af3509788617084525b63e3b]]
    Former-commit-id: 3cba4a8cfd23c2a2ee52a66eb4ac0cb2e787ead6
    Former-commit-id: 36a04f7efb4f3c33727c79ff6bff19dd5d06f4fd

--- Haskell/8.hs
+++ Haskell/8.hs
@@ -1,19 +1,16 @@
-module Eight where
-
 main :: IO ()
 main = do
-    anum <- readFile "../Data/8.txt"
-    let alist = map (read . (:"")) anum :: [Integer]
-    print $ gp13 alist
+  anum <- readFile "../Data/8.txt"
+  let alist = map (read . (: "")) anum :: [Integer]
+  print $ gp13 alist
 
 gp13 :: [Integer] -> Integer
-gp13 xs@(n:ns)
-    = gp13' xs ns
-        where
-            ps :: [Integer] -> Integer
-            ps xs = product (take 13 xs)
-            gp13' :: [Integer] -> [Integer] -> Integer
-            gp13' as bs | length bs <= 13 = max (ps as) (ps bs)
-                        | length as <= 13 = max (ps as) (ps bs)
-                        | ps as >= ps bs  = gp13' as (drop 1 bs)
-                        | otherwise       = gp13' (drop 1 as) bs
+gp13 xs@(n:ns) = gp13' xs ns
+ where
+  ps :: [Integer] -> Integer
+  ps xs = product (take 13 xs)
+  gp13' :: [Integer] -> [Integer] -> Integer
+  gp13' as bs | length bs <= 13 = max (ps as) (ps bs)
+              | length as <= 13 = max (ps as) (ps bs)
+              | ps as >= ps bs  = gp13' as (drop 1 bs)
+              | otherwise       = gp13' (drop 1 as) bs
