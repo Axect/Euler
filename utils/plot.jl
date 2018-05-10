6018b11dc9b07380071336bb1d7141634c4746c5
axect <edeftg@gmail.com>
2018-05-10 17:45:53 +0900

    Rust 8 Fix 2
    Former-commit-id: 1f32c24640140981790aca8ddf2dfe1bf08347e7 [formerly 1f32c24640140981790aca8ddf2dfe1bf08347e7 [formerly 6d3ad2a3ac4432f72fbf8be9349675f217e9f160]]
    Former-commit-id: b13feaedb86ddda26bcb89fc435047c2316be3dd
    Former-commit-id: e23b6029d6649cb060e343da6db6544860f1dc18

--- utils/plot.jl
+++ utils/plot.jl
@@ -1,14 +1,16 @@
 using Plots; gr()
 
+n = 8
+
 haskell = readcsv("Bench/toPlot/haskell.csv")
 nim = readcsv("Bench/toPlot/nim.csv")
 rust = readcsv("Bench/toPlot/rust.csv")
 
-plot(haskell[1:7,2], lab="haskell", w=3, alpha=0.5)
-plot!(nim[1:7,2], lab="nim", w=3, alpha=0.5)
-plot!(rust[1:7,2], lab="rust", w=3, alpha=0.5)
-xticks!(haskell[1:7,1])
-title!("Euler Benchmark (1~7)")
+plot(haskell[1:n,2], lab="haskell", w=3, alpha=0.5)
+plot!(nim[1:n,2], lab="nim", w=3, alpha=0.5)
+plot!(rust[1:n,2], lab="rust", w=3, alpha=0.5)
+xticks!(haskell[1:n,1])
+title!("Euler Benchmark (1~$n)")
 xaxis!("problem")
 yaxis!("time(s)")
 savefig("Bench/euler.png")
