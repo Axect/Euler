153c61a6d72078a6660c0b4a939ed001fce562d8
axect <edeftg@gmail.com>
2018-05-14 23:00:02 +0900

    Nim Fail Euler 11
    Former-commit-id: afdc34d53f1d01c12d134beaa196be435811e709 [formerly afdc34d53f1d01c12d134beaa196be435811e709 [formerly 40b8e7a4e8a5e43560f01fd89758e5396ceb9fdc]]
    Former-commit-id: 60214623e6869f31f84a1d8bcf54e3841d06149d
    Former-commit-id: c899d880ce9fe3b38a87e343216aec9df59da6a4

--- Nim/p011.nim
+++ Nim/p011.nim
@@ -9,7 +9,7 @@ proc hProd(row: Row): int =
   for i in 0..(row.high - 4):
     var s = 1
     for j in i..i+3:
-      s *= j
+      s *= row[j]
     if largest < s:
       largest = s
   return largest
@@ -27,10 +27,41 @@ proc hProduct(mat: Matrix): int =
 proc vProduct(mat: Matrix): int =
   hProduct(transpose(mat))
 
+proc diag(mat: Matrix): Row =
+  var temp: seq[int] = @[]
+  for i in 0..mat.high:
+    temp.add(mat[i][i])
+  return temp
+
+proc permut(mat: Matrix): Matrix =
+  var temp = mat
+  temp.delete(0)
+  temp.insert(mat[0], mat.high)
+  return temp
+
+proc completeSwap(mat: Matrix): Matrix =
+  var temp = mat
+  for i in 0..mat.high:
+    for j in 0..mat[0].high:
+      temp[i][j] = mat[i][len(mat[0]) - 1 - j]
+  return temp
+
+proc dProduct(mat: Matrix): int =
+  var temp = hProd(diag(mat))
+  var largest = temp
+  for i in 0..mat.high:
+    let temp_mat = permut(mat)
+    temp = hProd(diag(temp_mat))
+    if largest < temp:
+      largest = temp
+  return largest
+
 proc main =
   let temp = readFile("Data/11.txt")
-  let data = temp.split({'\n'}).filter(x => x != "").map(x => x.split({' '}))
-  echo data
+  let strdata = temp.split({'\n'}).filter(x => x != "").map(x => x.split({' '}))
+  let data = strdata.map(x => x.map(y => parseInt(y)))
+  echo @[hProduct(data), vProduct(data), dProduct(data), dProduct(completeSwap(data))]
+  echo hProd(@[1,2,5,26,63,78,14,3,7,6])
 
 if isMainModule:
   main()
