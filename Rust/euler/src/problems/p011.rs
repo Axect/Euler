64bf28645efee3d145bf4f7f71d9c61f07ae9014
Axect <edeftg@gmail.com>
2018-09-21 16:10:33 +0900

    Hmm
    Former-commit-id: 49078094e4760f2ed8ed280b11c266db160f90fa [formerly 44a34f20234eeda0960bf9544a9639de4cd58345]
    Former-commit-id: 4d505a121f023c589f365c28ae354138b5a2efce
    Former-commit-id: 0d8adb7d83e1a52ce88ffd72fe82ea69363388b6

--- Rust/euler/src/problems/p011.rs
+++ Rust/euler/src/problems/p011.rs
@@ -1,22 +1,19 @@
-pub fn ans() -> u64 {
+pub fn ans() {
     
 }
 
-//Type aliases
-type Row = Vec<u64>;
-type Matrix = Vec<Vec<u64>>;
-
-trait RowOperation {
-    fn large_product(&self) -> u64;
+pub struct Matrix<T> {
+    i: u64,
+    j: u64,
+    data: Vec<Item>,
 }
 
-trait MatrixOperation {
-    fn transpose(&self) -> Matrix;
-    fn swap_complete(&self) -> Matrix;
-    fn diagonal(&self) -> Row;
-    fn cyclic(&self) -> Matrix;
-    fn horizontal_product(&self) -> u64;
-    fn vertical_product(&self) -> u64;
-    fn diagonal_product(&self) -> u64;
-    fn reverse_diagonal_product(&self) -> u64;
+impl Matrix<T> {
+    pub fn new(x: u64, y:u64, v: Vec<T>) -> Matrix {
+        Matrix {
+            i: x,
+            j: y,
+            data: v,
+        }
+    }
 }
