86471a85b88c071c9ed353203cdae0aeab555d47
Tae Geun, Kim <edeftg@gmail.com>
2018-11-30 07:45:00 +0900

    Problem 11 Finished
    Former-commit-id: 57db30995c1f5d285a21b4616de6a39689e4119d [formerly cb768ff6eac760583a0da32455d54cd831f17bdd]
    Former-commit-id: 885b7d2b08bae8899b8ba44c635d395932a101f6
    Former-commit-id: 0d877ae32af2c0cccfff333229b84229103d73f7

--- Rust/euler/src/bin/p011.rs
+++ Rust/euler/src/bin/p011.rs
@@ -1,10 +1,101 @@
 extern crate peroxide;
 use peroxide::*;
+use std::process::exit;
 
 fn main() {
-    let a = c!(1,2,3,4,5);
-    a.print();
-
     let b = Matrix::read("data/p011.csv", false, ' ');
-    b.unwrap().print();
+    let c = match b {
+        Ok(m) => m,
+        Err(e) => {
+            println!("{}", e);
+            exit(1)
+        },
+    };
+    c!(c.h_product(), c.v_product(), c.d_product(), c.r_product()).reduce(0, |x,y| x.max(y)).print();
+}
+
+trait Problem11Vector {
+    fn product(&self) -> f64;
+}
+
+impl Problem11Vector for Vector {
+    fn product(&self) -> f64 {
+       let mut max = 1f64;
+
+        for i in 0 .. (self.len()-4) {
+            let mut p = 1f64;
+            for j in i .. (i+4) {
+                p *= self[j];
+            }
+            max = if p > max { p } else { max };
+        }
+        return max;
+    }
+}
+
+trait Problem11Matrix {
+    fn h_product(&self) -> f64;
+    fn v_product(&self) -> f64;
+    fn d_product(&self) -> f64;
+    fn r_product(&self) -> f64;
+}
+
+impl Problem11Matrix for Matrix {
+    fn h_product(&self) -> f64 {
+        let r = self.row;
+        let mut max = 1f64;
+        for i in 0 .. r {
+            let p = self.row(i).product();
+            max = if p > max { p } else { max };
+        }
+        return max;
+    }
+
+    fn v_product(&self) -> f64 {
+        let c = self.col;
+        let mut max = 1f64;
+        for i in 0 .. c {
+            let p = self.col(i).product();
+            max = if p > max { p } else { max };
+        }
+        return max;
+    }
+
+    fn d_product(&self) -> f64 {
+        let n = self.row;
+        assert_eq!(self.row, self.col);
+
+        let mut max = 1f64;
+
+        for i in 0 .. n {
+            for j in 0..(n-4) {
+                let mut p = 1f64;
+                for k in j .. j+4 {
+                    let idx = (i + k) % n;
+                    p *= self[(idx, k)];
+                }
+                max = if p > max { p } else { max };
+            }
+        }
+        return max;
+    }
+
+    fn r_product(&self) -> f64 {
+        let n = self.row;
+        assert_eq!(self.row, self.col);
+
+        let mut max = 1f64;
+
+        for i in 0 .. n {
+            for j in 0..(n-4) {
+                let mut p = 1f64;
+                for k in j .. j+4 {
+                    let idx = (i + k) % n;
+                    p *= self[(n-idx-1, k)];
+                }
+                max = if p > max { p } else { max };
+            }
+        }
+        return max;
+    }
 }
