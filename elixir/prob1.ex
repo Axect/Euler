f42666942468389d9d1fee59fffc6792a7473db2
Tae Geun, Kim <edeftg@gmail.com>
2018-12-10 03:51:30 +0900

    Elixir - Prob3
    Former-commit-id: 42449197c3689b84d11424a1baca4ee8ffbee5a0 [formerly c3a10c3416a1dc0ae846263f506f8202ce6b0fec]
    Former-commit-id: 2ab9a5ab6fb6fcf213b93126d60eb7b6e3438dbb
    Former-commit-id: 500cff8488ff50b566113634e16ad616c4f3edc2

--- elixir/prob1.ex
+++ elixir/prob1.ex
@@ -3,6 +3,6 @@ defmodule Prob1 do
         (1 .. 999)
             |> Enum.filter(fn(x) -> rem(x, 3) == 0 || rem(x, 5) == 0 end)
             |> Enum.reduce(fn(x, y) -> x + y end)
-            |> IO.puts
+            |> IO.inspect
     end
 end
