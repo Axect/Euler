9cd3958f9162aa3f3f7d1d53effeb3b930686b25
Tae Geun, Kim <edeftg@gmail.com>
2018-12-15 15:33:47 +0900

    Rust - Prob 12
    Former-commit-id: fdd9f586ef904eea4e31bfa643e8fa8716b3573d [formerly 78a3bb4f162b5e53e32174df375ad910929e9aee]
    Former-commit-id: 0aad59c5799d6b35e483ba6ff0e7c3f346a59769
    Former-commit-id: eafb585c8fe94c47486bc1c96df05dfbe14c4e0b

--- Rust/euler/src/problems/p012.rs
+++ Rust/euler/src/problems/p012.rs
@@ -1,21 +1,35 @@
-use crate::problems::tools::prime_factors;
+use crate::problems::tools::Prime;
 
 pub fn ans() {
-    println!("{:?}", triangular_gen(7));
-    println!("{}", number_of_divisors(36));
+    // a_n = 1 + Σ(n+1) = 1 + n(n-1)/2 + n-1 = n(n+1)/2
+    let result: u64 = (1..)
+        .map(|x| x * (x+1) / 2)
+        .skip_while(|&x| number_of_divisors(x) < 500)
+        .take(1)
+        .sum();
+
+    println!("{}", result);
 }
 
-fn triangular_gen(n: u64) -> Vec<u64> {
-    let mut s: u64 = 0;
-    let mut v: Vec<u64> = vec![0u64; n as usize];
-    for i in 1 .. n+1 {
-        s += i;
-        v[(i-1) as usize] = s;
+fn number_of_divisors(a: u64) -> u64 {
+    let mut div: u64 = 1;
+    let b = (a as f64).sqrt() as u64;
+
+    for p in gen_prime().take_while(|&p| p < b) {
+        let mut x = a;
+        let mut count: u64 = 0;
+        while x % p == 0 && x >= p {
+            x /= p;
+            count += 1;
+        }
+        div *= count + 1;
+        if x < p {
+            break;
+        }
     }
-    v
+    div
 }
 
-fn number_of_divisors(x: u64) -> usize {
-    let p = prime_factors(x);
-    p.len()
-} 
+fn gen_prime() -> Prime {
+    Prime { num: 1 }
+}
