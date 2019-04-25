960eb1adf9e4157d67ad6ff47d7fad373d463e65
axect <axect@outlook.kr>
2019-04-26 01:07:44 +0900

    Problem 20 - New Rust (Ramp Integration)

--- Rust/euler/src/problems/p020.rs
+++ Rust/euler/src/problems/p020.rs
@@ -1,3 +1,20 @@
-pub fn ans() {
+extern crate ramp;
+use ramp::Int;
 
+pub fn ans() {
+    let mut a = Int::from(1);
+    for i in 2 .. 101 {
+        a *= i;
+    }
+    println!("{}", digit_sum(a));
+}
+
+fn digit_sum(a: Int) -> usize {
+    let mut s = 0usize;
+    let mut b = a.clone();
+    while b > 10 {
+        s += usize::from(&(b.clone() % 10));
+        b /= 10;
+    }
+    s + usize::from(&b)
 }
