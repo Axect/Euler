d20b4c7015a42b5720dd47b50ac2d9ad005c16d8
axect <axect@outlook.kr>
2019-06-07 19:09:01 +0900

    Problem 2 - C++ Generator

--- C++/p002.cpp
+++ C++/p002.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
-int main() {
-    unsigned i=1, j=1;
-    unsigned s = 0;
-
-    while (i < 4000000u) {
-        i += j;
-        std::swap(i, j);
-        s += (i%2 == 0) ? i : 0;
+class EvenFib {
+public:
+    EvenFib(int s1, int s2) : curr(s1), next(s2) {}
+    void operator()() {
+        curr += next;
+        std::swap(curr, next);
+        while (curr % 2 == 1)
+            (*this)();
     }
+    int& get_curr() {
+        return this->curr;
+    }
+private:
+    int curr, next;
+};
+
+int main() {
+    EvenFib f(1, 2);
+    f(); // Find First even
+    int& curr = f.get_curr();
+    long s = 0;
+
+    while (curr <= 4000000) {
+        s += curr;
+        f();
+    }
+
     std::cout << s << std::endl;
 }
